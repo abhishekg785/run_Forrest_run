658892bff86edda93cc22313d377cab6f13e4cc0
abhishekg785 <abhishekg785@gmail.com>
2016-11-21 22:15:30 +0530

    enemy made visible to the game world

--- core/src/com/mygdx/martianrun/actors/Enemy.java
+++ core/src/com/mygdx/martianrun/actors/Enemy.java
@@ -1,6 +1,8 @@
 package com.mygdx.martianrun.actors;
 
+import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.graphics.g2d.Animation;
+import com.badlogic.gdx.graphics.g2d.Batch;
 import com.badlogic.gdx.graphics.g2d.TextureAtlas;
 import com.badlogic.gdx.graphics.g2d.TextureRegion;
 import com.badlogic.gdx.physics.box2d.Body;
@@ -19,7 +21,20 @@ public class Enemy extends GameActor {
 
         super(body);
         TextureAtlas textureAtlas = new TextureAtlas(Constants.CHARACTERS_ATLAS_PATH);
-//        TextureRegion[]
+        TextureRegion[] runningFrames = new TextureRegion[getUserData().getTextureRegions().length];
+        for( int i = 0 ; i < getUserData().getTextureRegions().length; i++ ){
+            String path = getUserData().getTextureRegions()[i];
+            runningFrames[i] = textureAtlas.findRegion(path);
+        }
+        animation = new Animation(0.1f, runningFrames);
+        stateTime = 0f;
+    }
+
+    @Override
+    public void draw(Batch batch, float parentAlpha) {
+        super.draw(batch, parentAlpha);
+        stateTime += Gdx.graphics.getDeltaTime();
+        batch.draw(animation.getKeyFrame(stateTime, true), (screenRectangle.x - (screenRectangle.width * 0.1f)), screenRectangle.y, screenRectangle.width * 1.2f, screenRectangle.height * 1.1f);
     }
 
     @Override
