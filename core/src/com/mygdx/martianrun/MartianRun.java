ce3ad55b687de72e4aa6113ccc4b524657e530d2
abhishekg785 <abhishekg785@gmail.com>
2016-12-12 19:52:04 +0530

    start screen added

--- core/src/com/mygdx/martianrun/MartianRun.java
+++ core/src/com/mygdx/martianrun/MartianRun.java
@@ -1,13 +1,30 @@
 package com.mygdx.martianrun;
 
 import com.badlogic.gdx.Game;
+import com.badlogic.gdx.graphics.g2d.BitmapFont;
+import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 import com.mygdx.martianrun.screens.GameScreen;
+import com.mygdx.martianrun.screens.MainMenuScreen;
 
 public class MartianRun extends Game {
-	
+
+	public SpriteBatch batch;    // Draw batched quads using indices
+	public BitmapFont font;
+
 	@Override
 	public void create () {
-		setScreen(new GameScreen());
+		batch = new SpriteBatch();
+		font = new BitmapFont();
+		setScreen(new MainMenuScreen( this ));
+	}
+
+	public void render() {
+		super.render();
+	}
+
+	public void dispose() {
+		batch.dispose();
+		font.dispose();
 	}
 
 }
