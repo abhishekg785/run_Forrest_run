0e80042fc961e1df2bbe3dc901361ad02a821b1d
abhishekg785 <abhishekg785@gmail.com>
2016-12-12 17:25:19 +0530

    screen for main menu added

---
+++ core/src/com/mygdx/martianrun/screens/MainMenuScreen.java
@@ -0,0 +1,44 @@
+package com.mygdx.martianrun.screens;
+
+import com.badlogic.gdx.Screen;
+
+/**
+ * Created by hiro on 12/12/16.
+ */
+public class MainMenuScreen implements Screen {
+
+    @Override
+    public void render(float delta) {
+        System.out.println("in the render function of the ")
+    }
+
+    @Override
+    public void show() {
+
+    }
+
+    @Override
+    public void resize(int width, int height) {
+
+    }
+
+    @Override
+    public void pause() {
+
+    }
+
+    @Override
+    public void resume() {
+
+    }
+
+    @Override
+    public void hide() {
+
+    }
+
+    @Override
+    public void dispose() {
+
+    }
+}
